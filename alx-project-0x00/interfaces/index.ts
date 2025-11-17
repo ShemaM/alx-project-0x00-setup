0309ac946aaebd4153386d82cf94f9468abe716b
Shema Manasseh <shemamanase992@gmail.com>
2025-11-17 11:48:37 +0300

    fixed syntax erros in interfaces/index.tsx

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,8 +1,8 @@
-export interfaces PillProps{
+export interface PillProps {
     title: string
 }
 
 export interface ButtonProps {
-  title: string;
-  styles?: string; // Optional class strings for tailwind or custom CSS
+    title: string;
+    styles?: string; // Correctly defined as optional
 }
